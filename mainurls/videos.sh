3e48a91eb3d0eabaa647b41c40092e5c27a27361
leotechnosoftgithub <64788555+leotechnosoftgithub@users.noreply.github.com>
2020-07-28 16:38:47 +0530

    asdf
    asdf

--- mainurls/videos.sh
+++ mainurls/videos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dat=$1
 key=/root/.ssh/mst.pem
-curl -o ./ips/veeresh  http://18.207.24.43/veeresh
+#curl -o ./ips/veeresh  http://18.207.24.43/veeresh
 for i in `cat xaa/gaa`;do ip=`shuf -n 1 ./ips/veeresh`; echo $ip; ssh -o StrictHostKeyChecking=no -i $key root@$ip ./veeresh.sh "\"$i\"" "\"$dat\"";done
 echo Subject: Job job1 completed | sendmail sheetal.pant@leotechnosoft.net
 screen -X quit
